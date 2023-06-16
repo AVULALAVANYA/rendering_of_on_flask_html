d4b8d3057bfce25a0e14e4ba2d9ac2f75aece1cd
Avula Lavanya <arlavanya123@gmail.com>
2023-06-16 11:16:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+FAI=Flask(__name__)
+@FAI.route('/first')
+def first():
+    return render_template('first.html')
+
+@FAI.route('/data_rendered')
+def data_rendered():
+    return render_template('data_rendered.html',name='Lavu',age=23)
+
+if __name__=='__main__':
+    FAI.run(debug=True)
